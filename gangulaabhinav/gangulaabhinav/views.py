91c628d456b8abba0c4844449de2b4f9abde591e
Abhinav Gangula <gangulaabhinav@gmail.com>
2020-05-18 00:16:50 +0530

    Add ListOfLinks.html template. Rendering gangulaabhinav home page with list of links. LinkedIn, GitHub, Facebook, Zomata and IMDb.

--- gangulaabhinav/gangulaabhinav/views.py
+++ gangulaabhinav/gangulaabhinav/views.py
@@ -1,6 +1,16 @@
+from flask import render_template
+
 from gangulaabhinav import app
 
 @app.route('/')
-def hello():
-    """Renders a sample page."""
-    return "Hello World!"
+@app.route('/home')
+def home():
+    #            Name        URL
+    myLinks = [["LinkedIn", "https://www.linkedin.com/in/abhinav-gangula-b0872462/"],
+               ["GitHub",   "https://github.com/gangulaabhinav"],
+               ["Facebook", "https://www.facebook.com/agangula"],
+               ["Zomato",   "https://www.zomato.com/agangula"],
+               ["IMDb",     "https://www.imdb.com/user/ur50420048"]]
+    return render_template("ListOfLinks.html",
+                           title = "Abhinav Gangula",
+                           links = myLinks)
