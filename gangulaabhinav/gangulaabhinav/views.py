af6f72b410757bbdc61f66fa212ac74fe2a777a1
Abhinav Gangula <gangulaabhinav@gmail.com>
2020-05-22 23:55:31 +0530

    Getting favicon argument from python render_template. Also adding box3d.ico for /3d

--- gangulaabhinav/gangulaabhinav/views.py
+++ gangulaabhinav/gangulaabhinav/views.py
@@ -1,3 +1,5 @@
+from os import path
+
 from flask import render_template
 
 from gangulaabhinav import app
@@ -14,7 +16,8 @@ def Home():
                ["IMDb",     "https://www.imdb.com/user/ur50420048"]]
     return render_template("ListOfLinks.html",
                            title = "Abhinav Gangula",
-                           links = myLinks)
+                           links = myLinks,
+                           icon = path.join("static", "gangulaabhinav.ico"))
 
 # There might be a conflict on removing /hello from url and using <name> directly. Conflict with /3d
 @app.route('/hello', defaults={'name': 'World'})
@@ -22,13 +25,15 @@ def Home():
 def Hello(name = "World"):
     return render_template("Body.html",
                            title = "Hello",
-                           content = "Hello " + name)
+                           content = "Hello " + name,
+                           icon = path.join("static", "gangulaabhinav.ico")) # favico not working here. hello is being added to favicon path
 
 @app.route('/3d')
 def Box3d():
     try:
         return render_template("Body.html",
                                title = "Box3d",
-                               content = Get3dSnippet())
+                               content = Get3dSnippet(),
+                               icon = path.join("static", "box3d.ico"))
     except Exception as e:
         return(str(e))  
